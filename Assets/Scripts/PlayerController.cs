6e1161efa5623af966fb2547bbafa7277dff611a
Gerrard Lukacs <gerrard.lukacs@stonybrook.edu>
2016-03-13 09:55:24 -0400

    Outcome of first class for this lesson.
    Imported level, fixed collisions, and added stairs and an extra platform.
    Imported FPS controller.
    Began working on our own FPS controller.
    Played around with simple Blender models.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    CharacterController characterController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0f,
+                                             Input.GetAxis("Vertical"));
+        characterController.Move(transform.rotation * movementVector * speed
+                                 * Time.deltaTime);
+	}
+}
