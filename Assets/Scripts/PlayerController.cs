f9af21a4c0190a60695393b3a5e8975857b7553e
WolfieMario <wolfiemario@hotmail.com>
2016-03-13 13:29:44 -0400

    Updated movement script. Added gravity.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,22 +3,45 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpSpeed = 10f;
 
     CharacterController characterController;
 
 	// Use this for initialization
-	void Start ()
+	void Awake()
     {
         characterController = GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
-	void Update ()
+	void FixedUpdate()
     {
-        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0f,
-                                             Input.GetAxis("Vertical"));
-        characterController.Move(transform.rotation * movementVector * speed
-                                 * Time.deltaTime);
+        Vector2 movementInput = GetHorizontalMovement();
+        Vector3 movementVector = transform.forward * movementInput.y
+                               + transform.right * movementInput.x;
+
+        movementVector += Physics.gravity;
+
+        characterController.Move(movementVector * Time.deltaTime);
 	}
+
+    Vector2 GetHorizontalMovement()
+    {
+        // Read player input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float speed;
+        if (isRunning)
+            speed = runSpeed;
+        else
+            speed = walkSpeed;
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        if (movement.sqrMagnitude > 1)
+            movement.Normalize();
+        return movement * speed;
+    }
 }
