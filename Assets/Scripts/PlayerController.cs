4a061322e1affe9d1965da75f877a157b343781a
WolfieMario <wolfiemario@hotmail.com>
2016-03-20 14:57:51 -0400

    Lecture 3 result - functioning shooting.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,25 @@ public class PlayerController : MonoBehaviour
     public float runSpeed = 10f;
     public float jumpSpeed = 10f;
 
+    public Vector2 mouseSensitivity = new Vector2(2, 2);
+    public float minimumXRotation = -90;
+    public float maximumXRotation = 90;
+
     CharacterController characterController;
+    Camera camera;
 
 	// Use this for initialization
 	void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        camera = Camera.main;
 	}
+
+    void Update()
+    {
+        MouseLook();
+        UpdateCursorLock();
+    }
 	
 	void FixedUpdate()
     {
@@ -21,14 +33,70 @@ public class PlayerController : MonoBehaviour
         Vector3 movementVector = transform.forward * movementInput.y
                                + transform.right * movementInput.x;
 
-        movementVector += Physics.gravity;
+        if (!characterController.isGrounded)
+            movementVector.y = characterController.velocity.y;
+        movementVector += Physics.gravity * Time.deltaTime;
 
         float isJumping = Input.GetAxis("Jump");
-        if (isJumping > 0f)
+        if (isJumping > 0f && characterController.isGrounded)
             movementVector.y = jumpSpeed;
 
         characterController.Move(movementVector * Time.deltaTime);
 	}
+
+    void MouseLook()
+    {
+        float rotationY = Input.GetAxis("Mouse X") * mouseSensitivity.x;
+        float rotationX = Input.GetAxis("Mouse Y") * mouseSensitivity.y;
+
+        characterController.transform.localRotation *= Quaternion.Euler(0f, rotationY, 0f);
+        camera.transform.localRotation = ClampRotationAroundXAxis(camera.transform.localRotation * Quaternion.Euler(-rotationX, 0f, 0f));
+
+//        Quaternion cameraRotation = camera.transform.localRotation * Quaternion.Euler(-rotationX, 0f, 0f);
+//		Vector3 cameraEulerRotation = cameraRotation.eulerAngles;
+//        cameraEulerRotation.x = Mathf.Clamp((cameraEulerRotation.x + 180) % 360 - 180,
+//                                            minimumXRotation, maximumXRotation);
+//        cameraEulerRotation.y = 0f;
+//        cameraEulerRotation.z = 0f;
+//        cameraRotation.eulerAngles = cameraEulerRotation;
+//        camera.transform.localRotation = cameraRotation;
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+
+        angleX = Mathf.Clamp (angleX, minimumXRotation, maximumXRotation);
+
+        q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    void UpdateCursorLock()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            UnlockMouse();
+        else if (Input.GetMouseButton(0))
+            LockMouse();
+    }
+
+    public void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
 
     Vector2 GetHorizontalMovement()
     {
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    new Camera camera;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Fire1") > 0) {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(camera.transform.position, camera.transform.forward, out hitInfo);
+            GameObject enemy = hitInfo.transform.gameObject;
+            if (hit && enemy.tag == "Enemy") {
+                GameObject ragdoll = enemy.transform.parent.FindChild("Ragdoll").gameObject;
+                Object.Destroy(enemy);
+                ragdoll.SetActive(true);
+            }
+        }
+	}
+}
