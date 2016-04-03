ff75250560118da16fa3e37c87c38085ee3da24b
WolfieMario <wolfiemario@hotmail.com>
2016-04-03 15:47:24 -0400

    Lecture 4 main result: Implemented enemy facing player, holding gun, and aiming it.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    Animator animator;
+    GameObject player;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rotation = transform.parent.rotation;
+        Vector3 direction = player.transform.position -
+                            transform.parent.position;
+        direction.y = 0;
+        rotation.SetLookRotation(direction, Vector3.up);
+        transform.parent.rotation = rotation;
+    }
+
+    void OnAnimatorIK(int layer)
+    {
+        animator.SetIKPosition(AvatarIKGoal.RightHand, player.transform.position);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+    }
+}
